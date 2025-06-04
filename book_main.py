a0a7a1f22a5ad0d12a6be84bffd533668f4e425c
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-04 11:24:05 +0100

    update book

--- book_main.py
+++ book_main.py
@@ -3,7 +3,7 @@ from fastapi.exceptions import HTTPException
 from typing import List
 
 from book_list import books
-from book_models import Book
+from book_models import Book, BookUpdate
 
 app = FastAPI()
 
@@ -32,8 +32,19 @@ async def get_book(book_id: int) -> dict:
 
 
 @app.patch("/books/{book_id}")
-async def update_book(book_id: int) -> dict:
-    pass
+async def update_book(book_id: int, book_update_data: BookUpdate) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            book["title"] = book_update_data.title
+            book["author"] = book_update_data.author
+            book["publisher"] = book_update_data.publisher
+            book["page_count"] = book_update_data.page_count
+            book["language"] = book_update_data.language
+            book["title"] = book_update_data.title
+
+            return book
+        
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 
 @app.delete("/books/{book_id}")
