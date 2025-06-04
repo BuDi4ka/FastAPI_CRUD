254b2e7de5c9c7c8a175ac59162dfbbf3538336b
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-04 10:34:42 +0100

    headers params

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from typing import Optional
 from pydantic import BaseModel
 
@@ -16,13 +16,30 @@ async def greet_name(name: Optional[str] = "User", age: int = 0) -> dict:
 
 
 class BookCreateModel(BaseModel):
-    title : str
-    author : str 
+    title: str
+    author: str
 
 
-@app.post('/create_book')
+@app.post("/create_book")
 async def create_book(book_data: BookCreateModel):
     return {
         "title": book_data.title,
         "author": book_data.author,
     }
+
+
+@app.get("/get_headers")
+async def get_headers(
+    accept: str = Header(None),
+    content_type: str = Header(None),
+    user_agent: str = Header(None),
+    host: str = Header(None),
+):
+    request_headers = {}
+
+    request_headers["Accept"] = accept
+    request_headers["Content-Type"] = content_type
+    request_headers["User-Agent"] = user_agent
+    request_headers["Host"] = host
+
+    return request_headers
