665e2e35d81a85464b927886a62c0b2ed8e6e101
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-04 10:13:14 +0100

    optional params

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get("/")
 async def read_root():
-    return {'message': "Hello World"}
+    return {"message": "Hello World"}
 
 
-@app.get('/greet/{name}')
-async def greet_name(name:str, age:int) -> dict:
-    return {'message': f'Hello {name}', 'age': age}
+@app.get("/greet")
+async def greet_name(name: Optional[str] = "User", age: int = 0) -> dict:
+    return {"message": f"Hello {name}", "age": age}
