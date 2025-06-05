fcf4b8ffe0dca3a769939f2fa680b8ee03c68949
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-05 16:31:21 +0100

    added book services

--- src/db/main.py
+++ src/db/main.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator
+
 from sqlmodel import text, SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -9,14 +11,14 @@ async_engine = create_async_engine(Config.DATABASE_URL, echo=True)
 
 
 async def init_db():
-    async with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         from src.books.models import Book
 
         await conn.run_sync(SQLModel.metadata.create_all)
         print("DB creation successful")
 
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     Session = sessionmaker(
         bind=async_engine, class_=AsyncSession, expire_on_commit=False
     )
