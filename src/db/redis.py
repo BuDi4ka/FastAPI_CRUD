04d75ba8bf5f842d49e2a3f8014205f408b28dd8
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-06 17:34:03 +0100

    revoking tokens fully working

--- src/db/redis.py
+++ src/db/redis.py
@@ -9,7 +9,7 @@ token_blocklist = Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=0)
 
 
 async def add_jti_to_blocklist(jti: str) -> None:
-    await token_blocklist.set(name=jti, value="", exp=JTI_EXPIRY)
+    await token_blocklist.set(name=jti, value="", ex=JTI_EXPIRY)
 
 
 async def check_jti_in_blocklist(jti: str) -> bool:
