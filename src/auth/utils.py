ad6c2e484d1967e9f51e24a2cd040e14d0a0bd7c
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-06 14:05:41 +0100

    login functionality

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -22,15 +22,17 @@ def verify_password(password: str, hash: str) -> bool:
     return password_context.verify(password, hash)
 
 
-def create_access_token(user_data: dict, expiry: timedelta = None):
+def create_access_token(
+    user_data: dict, expiry: timedelta = None, refresh: bool = False
+):
     payload = {}
 
     payload["user"] = user_data
     payload["exp"] = datetime.now() + (
         expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY)
     )
-    payload['jti'] = str(uuid.uuid4())
-    payload['refresh'] = refresh
+    payload["jti"] = str(uuid.uuid4())
+    payload["refresh"] = refresh
 
     token = jwt.encode(
         payload=payload,
@@ -44,13 +46,11 @@ def create_access_token(user_data: dict, expiry: timedelta = None):
 def decode_token(token: str) -> dict:
     try:
         token_data = jwt.decode(
-            jwt=token,
-            key=Config.JWT_SECRET,
-            algorithms=[Config.JWT_ALGORITHM]
+            jwt=token, key=Config.JWT_SECRET, algorithms=[Config.JWT_ALGORITHM]
         )
 
         return token_data
-    
+
     except jwt.PyJWTError as e:
         logging.exception(e)
         return None
