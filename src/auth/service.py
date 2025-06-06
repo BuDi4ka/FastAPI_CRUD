7a3cf0a7cec0d1b2ab7429b0e4216325d568678b
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-06 12:26:02 +0100

    services and utils for user

--- src/auth/service.py
+++ src/auth/service.py
@@ -3,6 +3,7 @@ from sqlmodel import select
 
 from .models import User
 from .schemas import UserCreateModel
+from .utils import generate_password_hash
 
 
 class UserService:
@@ -19,10 +20,14 @@ class UserService:
         user = await self.get_user_by_email(email, session)
 
         return True if user is not None else False
-    
+
     async def create_user(self, user_data: UserCreateModel, session: AsyncSession):
         user_data_dict = user_data.model_dump()
 
-        new_user = User(
-            **user_data_dict
-        )
+        new_user = User(**user_data_dict)
+        new_user.password_hash = generate_password_hash(user_data_dict["password"])
+
+        session.add(new_user)
+        await session.commit()
+
+        return new_user
