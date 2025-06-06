d4d01184f4c8db0d42e7d54a5cbd6edaa08f28a3
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-06 12:21:29 +0100

    added simple user service

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
+from .schemas import UserCreateModel
+
 
 auth_router = APIRouter()
 
 
 @auth_router.post('/signup')
-async def create_user_account():
+async def create_user_account(user_data: UserCreateModel):
     pass
