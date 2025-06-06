a0d618c38e7aeff45f96aced9f4490e07512cad8
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-06 12:54:20 +0100

    working creating user feature

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,7 +1,24 @@
 from pydantic import BaseModel, Field
 
+from uuid import UUID
+from datetime import datetime
+
 
 class UserCreateModel(BaseModel):
     username: str = Field(max_length=8)
     email: str = Field(max_length=40)
+    first_name: str
+    last_name: str
     password: str = Field(min_length=6)
+
+
+class UserResponseModel(BaseModel):
+    uid: UUID
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool 
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    updated_at: datetime
