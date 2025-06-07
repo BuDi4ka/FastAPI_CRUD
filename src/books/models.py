2bcc53353a955d1ab389b470fb75a102d8297b31
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-07 10:57:53 +0100

    added user_id field to book model, foreign key

--- src/books/models.py
+++ src/books/models.py
@@ -3,7 +3,7 @@ import sqlalchemy.dialects.postgresql as pg
 from uuid import UUID, uuid4
 
 from datetime import datetime, date
-
+from typing import Optional
 
 class Book(SQLModel, table=True):
     __tablename__ = "books"
@@ -17,6 +17,7 @@ class Book(SQLModel, table=True):
     published_date: date
     page_count: int
     language: str
+    user_uid: Optional[UUID] = Field(default=None, foreign_key='users.uid')
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
 
